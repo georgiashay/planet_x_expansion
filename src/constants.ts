9f7cd216bd79d959bcfb55297f564036bfdeb7f4
Georgia Shay <georgiashay@gmail.com>
2021-05-09 00:11:19 -0400

    Use planet x server

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
-// export const SERVER_URL = "https://planetx.shaytech.net";
-export const SERVER_URL = "localhost:8000";
-export const API_URL = "http://" + SERVER_URL;
+export const SERVER_URL = "planetx.shaytech.net";
+// export const SERVER_URL = "localhost:8000";
+const SECURE = true;
+const URL_PREFIX = SECURE ? "https://" : "http://";
+export const API_URL = URL_PREFIX + SERVER_URL;
+console.log(API_URL);
 export const WEBSOCKET_URL = "ws://" + SERVER_URL;
 export const GAME_TYPES: {[sector: number]: any} = {
   12: {
